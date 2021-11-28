31fc2efb943559722572358518e6d308fc5729af
LuciLua <luci.lua81@gmail.com>
2021-11-27 23:00:02 -0300

    🔴 Create card for put pokemon data

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "../styles/Home.module.css";
-import Image from "next/image"
+import Image from "next/image";
 
 export async function getStaticProps(ctx) {
   const url = "https://pokeapi.co/api/v2/pokedex/2/";
@@ -19,29 +19,59 @@ export async function getStaticProps(ctx) {
 export default function Home(props) {
   const { pokemons } = props;
 
+  const myLoader = () => {
+    return `https://assets.pokemon.com/assets/cms2/img/misc/countries/pt/country_detail_pokemon.png`;
+  };
+
   return (
     <>
       <div className={styles.container}>
         <h1>Pokedéx - LuciLua</h1>
         <div className={styles.wrap}>
-        <table className={styles.tablePokemon}>
-          <tr>
-            <th>ID</th>
-            <th>Nome</th>
-          </tr>
-          {pokemons.map((pokemon) => (
+          <table className={styles.tablePokemon}>
             <tr>
-              <td key={pokemon.entry_number}>{pokemon.entry_number}</td>
-              <td key={pokemon.entry_number}>{pokemon.pokemon_species.name}</td>
+              <th>ID</th>
+              <th>Nome</th>
             </tr>
-          ))}
-        </table>
-        <div className={styles.infoPokemon}>
-            <h1>
-                POKEMON INFO
-            </h1>
-            {/* <Image src={'https://pngimg.com/uploads/pokemon/pokemon_PNG154.png'}/> */}
-        </div>
+            {pokemons.map((pokemon) => (
+              <tr>
+                <td key={pokemon.entry_number}>{pokemon.entry_number}</td>
+                <td key={pokemon.entry_number}>
+                  {pokemon.pokemon_species.name}
+                </td>
+              </tr>
+            ))}
+          </table>
+          <div className={styles.infoPokemon}>
+            <h1>POKEMON INFO</h1>
+            <div className={styles.aboutPokemon}>
+              <div className={styles.imagemPokemon}>
+                <Image
+                  src="https://assets.pokemon.com/assets/cms2/img/misc/countries/pt/country_detail_pokemon.png"
+                  width={250}
+                  height={250}
+                  quality={100}
+                  loader={myLoader}
+                  objectFit="cover"
+                  alt="Picture of the author"
+                />
+              </div>
+              <ul>
+                  <li>
+                      Dado sobre pokemon
+                  </li>
+                  <li>
+                      Dado sobre pokemon
+                  </li>
+                  <li>
+                      Dado sobre pokemon
+                  </li>
+                  <li>
+                      Dado sobre pokemon
+                  </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </>

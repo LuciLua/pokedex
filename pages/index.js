f1ec41c5a01188525b8fd06d2714b2caabff8b19
LuciLua <luci.lua81@gmail.com>
2021-11-27 22:24:03 -0300

    🔴 Create visual structure for input data about each pokemon

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,51 @@
-import React, { useEffect, useState } from 'react'
-import styles from '../styles/Home.module.css'
+import React from "react";
+import styles from "../styles/Home.module.css";
+import Image from "next/image"
 
-export async function getStaticProps(ctx){
+export async function getStaticProps(ctx) {
+  const url = "https://pokeapi.co/api/v2/pokedex/2/";
+  const pokemons = await fetch(url)
+    .then((resp) => {
+      if (resp.ok) return resp.json();
+    })
+    .then((json) => json.pokemon_entries);
 
-    const url = 'https://pokeapi.co/api/v2/pokedex/2/'
-    const pokemons = await fetch(url)
-            .then(resp => { if (resp.ok) return resp.json() })
-            .then(json => json.pokemon_entries)
-
-    return { // props vai ser declarada na Home
-        props: {pokemons}
-    }
+  return {
+    // props vai ser declarada na Home
+    props: { pokemons },
+  };
 }
 
-export default function Home(props){
+export default function Home(props) {
+  const { pokemons } = props;
 
-    const { pokemons } = props
-
-    return(
-        <>
-        <div className={styles.container}>
-            <h1>Pokedéx - LuciLua</h1>
-            <table className={styles.tablePokemon}>
-                <tr>
-                <th>ID</th>
-                <th>Nome</th>
-                </tr>
-                <tr>
-                    <td>
-                    {pokemons.map(pokemon => (
-                        <tr key={pokemon.entry_number}>
-                            {pokemon.entry_number}
-                        </tr>
-                        ))}
-                    </td>
-                    <td>
-                    {pokemons.map(pokemon => (
-                        <tr key={pokemon.entry_number}>
-                            {pokemon.pokemon_species.name}
-                        </tr>
-                        ))}
-                    </td>
-                </tr>
-
-            </table>
-
+  return (
+    <>
+      <div className={styles.container}>
+        <h1>Pokedéx - LuciLua</h1>
+        <div className={styles.wrap}>
+        <table className={styles.tablePokemon}>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+          </tr>
+          {pokemons.map((pokemon) => (
+            <tr>
+              <td key={pokemon.entry_number}>{pokemon.entry_number}</td>
+              <td key={pokemon.entry_number}>{pokemon.pokemon_species.name}</td>
+            </tr>
+          ))}
+        </table>
+        <div className={styles.infoPokemon}>
+            <h1>
+                POKEMON INFO
+            </h1>
+            {/* <Image src={'https://pngimg.com/uploads/pokemon/pokemon_PNG154.png'}/> */}
         </div>
+        </div>
+      </div>
     </>
-    )
+  );
 }
 
 // https://pokeapi.co/

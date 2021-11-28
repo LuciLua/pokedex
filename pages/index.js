830ad0ef31ef30be4f09ed42d0dd3f0669f89042
LuciLua <luci.lua81@gmail.com>
2021-11-28 19:27:52 -0300

    🔴 toggle table and init layout for mobile

--- pages/index.js
+++ pages/index.js
@@ -33,30 +33,38 @@ export default function Home(props) {
     
   const { pokemons, pokemonsIMG } = props;
 
-  const myLoader = () => {
-    return `https://assets.pokemon.com/assets/cms2/img/misc/countries/pt/country_detail_pokemon.png`;
-  };
-
-
  function poke(e){
     var nomePoke = e.target
 
-      ReactDOM.render(`Nome: ${nomePoke.className}`, document.getElementById('tituloPoke'))
+    // ReactDOM.render(`Nome: ${nomePoke.className}`, document.getElementById('tituloPoke'))
+    // ReactDOM.render(`ID: ${nomePoke.id}`, document.getElementById('idPoke'))
+    document.getElementById('tituloPoke').innerHTML = `Nome: ${nomePoke.className}`
+    document.getElementById('idPoke').innerHTML = `ID: ${nomePoke.id}`
 
-      ReactDOM.render(`ID: ${nomePoke.id}`, document.getElementById('idPoke'))
-
-      var novoLink = pokemonsIMG.replace(idPoked, nomePoke.id)
-
+    var novoLink = pokemonsIMG.replace(idPoked, nomePoke.id)
     document.getElementById('pokeIma').src=novoLink
   }
+  
+  
+  function togglePokedex(o){
+    const table = document.getElementById('tablePokemon')
+    const btn = document.getElementById('btnTogglePokedex')
+
+    table.classList.toggle(styles.tablemenor) 
+
+  }
+
 
   return (
     
     <>
       <div className={styles.container}>
-        <h1>Pokedéx - LuciLua</h1>
+        <h1>Pokedéx</h1>
         <div className={styles.wrap}>
-          <table className={styles.tablePokemon}>
+          <button className={styles.btnTogglePokedex} id={'btnTogglePokedex'} onClick={togglePokedex}>
+            Pokedex
+          </button>
+          <table className={styles.tablePokemon} id={'tablePokemon'}>
             <thead>
               <tr>
                 <th>ID</th>
@@ -82,19 +90,20 @@ export default function Home(props) {
                   unoptimized
                   width={250}
                   height={250}
-                  quality={100}
-                  loader={myLoader}
                   objectFit="cover"
-                  alt="Picture of the author"
+                  alt="Picture of pokemon"
                 />
               </div>
               <ul>
-                  <li id={'tituloPoke'}></li>
-                  <li id={'idPoke'}></li>
+                  <li id={'tituloPoke'}>bulbasaur</li>
+                  <li id={'idPoke'}>1</li>
               </ul>
             </div>
           </div>
         </div>
+        <footer className={styles.footer}>
+        LuciLua@2021
+        </footer>
       </div>
     </>
   );

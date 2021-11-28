b3ecdbe7b994147b8ba0af3adac3aae327c86432
LuciLua <luci.lua81@gmail.com>
2021-11-28 01:03:34 -0300

    🔴 Imagem dos pokemons, id e nome

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,84 @@
 import React from "react";
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
+import  ReactDOM  from "react-dom";
+
+var idPoked = 1
 
 export async function getStaticProps(ctx) {
+
   const url = "https://pokeapi.co/api/v2/pokedex/2/";
+  const urlImg = `https://pokeapi.co/api/v2/pokemon/${idPoked}`;
+
   const pokemons = await fetch(url)
     .then((resp) => {
       if (resp.ok) return resp.json();
     })
     .then((json) => json.pokemon_entries);
 
+  const pokemonsIMG = await fetch(urlImg)
+    .then((resp) => {
+      if (resp.ok) return resp.json();
+    })
+    .then((json) => json.sprites.front_default);
+
   return {
     // props vai ser declarada na Home
-    props: { pokemons },
+    props: { pokemons, pokemonsIMG },
   };
 }
 
 export default function Home(props) {
-  const { pokemons } = props;
+
+    
+  const { pokemons, pokemonsIMG } = props;
 
   const myLoader = () => {
     return `https://assets.pokemon.com/assets/cms2/img/misc/countries/pt/country_detail_pokemon.png`;
   };
 
+
+ function poke(e){
+    var nomePoke = e.target
+
+      ReactDOM.render(`Nome: ${nomePoke.className}`, document.getElementById('tituloPoke'))
+
+      ReactDOM.render(`ID: ${nomePoke.id}`, document.getElementById('idPoke'))
+
+      var novoLink = pokemonsIMG.replace(idPoked, nomePoke.id)
+
+    document.getElementById('pokeIma').src=novoLink
+  }
+
   return (
+    
     <>
       <div className={styles.container}>
         <h1>Pokedéx - LuciLua</h1>
         <div className={styles.wrap}>
           <table className={styles.tablePokemon}>
-            <tr>
-              <th>ID</th>
-              <th>Nome</th>
-            </tr>
-            {pokemons.map((pokemon) => (
-              <tr key={pokemon.entry_number}>
-                <td>{pokemon.entry_number}</td>
-                <td>{pokemon.pokemon_species.name}</td>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Nome</th>
               </tr>
-            ))}
+            </thead>
+            <tbody>
+              {pokemons.map((pokemon) => (
+                <tr key={pokemon.entry_number} onClick={poke}>
+                  <td className={pokemon.pokemon_species.name} id={pokemon.entry_number}>{pokemon.entry_number} </td>
+                  <td className={pokemon.pokemon_species.name} id={pokemon.entry_number}>{pokemon.pokemon_species.name}</td>
+                </tr>
+              ))}
+            </tbody>
           </table>
           <div className={styles.infoPokemon}>
             <h1>POKEMON INFO</h1>
             <div className={styles.aboutPokemon}>
               <div className={styles.imagemPokemon}>
                 <Image
-                  src="https://assets.pokemon.com/assets/cms2/img/misc/countries/pt/country_detail_pokemon.png"
+                  id={'pokeIma'}
+                  src={pokemonsIMG}
                   unoptimized
                   width={250}
                   height={250}
@@ -56,18 +89,8 @@ export default function Home(props) {
                 />
               </div>
               <ul>
-                  <li key={`especie`}>
-                      Dado sobre pokemon 1
-                  </li>
-                  <li key={`pc`}>
-                      Dado sobre pokemon 2
-                  </li>
-                  <li key={`vida`}>
-                      Dado sobre pokemon 3
-                  </li>
-                  <li key={`poder`}> 
-                      Dado sobre pokemon 4
-                  </li>
+                  <li id={'tituloPoke'}></li>
+                  <li id={'idPoke'}></li>
               </ul>
             </div>
           </div>

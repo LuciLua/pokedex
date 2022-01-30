d45d9627c2d58d5e1dd229d9a8ec8dff6089d9ce
LuciLua <luci.lua81@gmail.com>
2022-01-30 15:49:40 -0300

    🔴 ProgressBar attack defense speed hp

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,46 +6,39 @@ var idPoked = 1
 
 export async function getStaticProps(ctx) {
 
-  const url = "https://pokeapi.co/api/v2/pokedex/2/";
-  const urlImg = `https://pokeapi.co/api/v2/pokemon/${idPoked}`;
+  const urlAllPokemons = "https://pokeapi.co/api/v2/pokedex/1/";
+  const urlData = `https://pokeapi.co/api/v2/pokemon/${idPoked}`;
 
-  const pokemons = await fetch(url)
+  const pokemons = await fetch(urlAllPokemons)
     .then((resp) => {
       if (resp.ok) return resp.json();
       throw new Error('Erro!')
     })
     .then((json) => json.pokemon_entries);
 
-
-  const pokemonsIMG = await fetch(urlImg)
+  const pokeData = await fetch(urlData)
     .then((resp) => {
       if (resp.ok) return resp.json();
+      throw new Error('Erro!')
     })
-    .then((json) => json.sprites.other.home.front_shiny);
+    .then((json) => json);
 
   return {
-    // props vai ser declarada na Home
-    props: { pokemons, pokemonsIMG },
+    props: { pokemons, pokeData },
   };
 }
 
 export default function Home(props) {
 
-
-  const { pokemons, pokemonsIMG } = props;
+  const { pokemons, pokeData } = props;
 
   function poke(e) {
     var select = e.target
-    var value = select.options[select.selectedIndex];
+    var nome = select.options[select.selectedIndex].text
 
-    document.getElementById('tituloPoke').innerHTML = value.text
+    var hpProgress = document.getElementById(styles.hpProgress)
 
-    pokemons.map( (pokemon) => 
-    {
-      pokemon.pokemon_species.name == value ? console.log(pokemon.stats[0]) : null
-    }
-    )
-
+    var tituloPokemon = document.getElementById('tituloPoke')
     var hp = document.getElementById('hp')
     var attack = document.getElementById('attack')
     var defense = document.getElementById('defense')
@@ -53,18 +46,20 @@ export default function Home(props) {
     var specialDefense = document.getElementById('specialDefense')
     var speed = document.getElementById('speed')
 
-    // hp.innerHTML = value.stats[0].base_stat
-    // attack.innerHTML = value.stats[1].base_stat
-    // defense.innerHTML = value.stats[2].base_stat
-    // specialAttack.innerHTML = value.stats[3].base_stat
-    // specialDefense.innerHTML = value.stats[4].base_stat
-    // speed.innerHTML = value.stats[5].base_stat
+    tituloPokemon.innerHTML = nome.toUpperCase()
 
+    hp.innerHTML = pokeData.stats[0].base_stat
+    attack.innerHTML = pokeData.stats[1].base_stat
+    defense.innerHTML = pokeData.stats[2].base_stat
+    specialAttack.innerHTML = pokeData.stats[3].base_stat
+    specialDefense.innerHTML = pokeData.stats[4].base_stat
+    speed.innerHTML = pokeData.stats[5].base_stat
 
-
-    // var novoLink = pokemonsIMG.replace(idPoked, nomePoke.id)
-    // document.getElementById('pokeIma').src = novoLink
   }
+
+  // var novoLink = urlData.replace(idPoked, novoPoke)
+  // document.getElementById('pokeIma').src = novoLink
+
 
   return (
     <>
@@ -78,19 +73,69 @@ export default function Home(props) {
           ))}
         </select>
         <div className={styles.infoPokemon}>
-          <h1>POKEMON INFO</h1>
+          <h1 id={'tituloPoke'}>BULBASAUR</h1>
           <div className={styles.aboutPokemon}>
             <div className={styles.imagemPokemon}>
-              <img id={'pokeIma'} src={pokemonsIMG} />
+              <img id={'pokeIma'} src={pokeData.sprites.other.home.front_shiny} />
             </div>
             <ul>
-              <li id={'tituloPoke'}>bulbasaur</li>
-              <li id={'hp'}>0</li>
-              <li id={'attack'}>0</li>
-              <li id={'defense'}>0</li>
-              <li id={'special-attack'}>0</li>
-              <li id={'special-defense'}>0</li>
-              <li id={'speed'}>0</li>
+              <div className={styles.dataArea}>
+                <label htmlFor="hp">
+                  HP
+                </label>
+                <li id={'hp'}>
+                  <span>0</span>
+                  <div className={styles.progressBar} id={styles.hpProgress}/>
+                </li>
+              </div>
+              <div className={styles.dataArea}>
+
+                <label htmlFor="attack">
+                  Attack
+                </label>
+                <li id={'attack'}>
+                  <span>0</span>
+                  <div id={styles.attackProgress} className={styles.progressBar}/>
+                </li>
+              </div>
+              <div className={styles.dataArea}>
+
+                <label htmlFor="defense">
+                  Defense
+                </label>
+                <li id={'defense'}>
+                  <span>0</span>
+                  <div id={styles.defenseProgress} className={styles.progressBar}/>
+                </li>
+              </div>
+              <div className={styles.dataArea}>
+                <label htmlFor="specialAttack">
+                  Special Attack
+                </label>
+                <li id={'specialAttack'}>
+                  <span>0</span>
+                  <div id={styles.specialAttackProgress} className={styles.progressBar}/>
+                </li>
+              </div>
+              <div className={styles.dataArea}>
+
+                <label htmlFor="specialDefense">
+                  Special Defense
+                </label>
+                <li id={'specialDefense'}>
+                  <span>0</span>
+                  <div id={styles.specialDefenseProgress} className={styles.progressBar}/>
+                </li>
+              </div>
+              <div className={styles.dataArea}>
+                <label htmlFor="speed">
+                  Speed
+                </label>
+                <li id={'speed'}>
+                  <span>0</span>
+                  <div id={styles.speedProgress} className={styles.progressBar}/>
+                </li>
+              </div>
             </ul>
           </div>
         </div>
